b41ccf90606ac8748f0b6e5a4127f07e959c4aa1
Saul Dominguez <saul.vau@gmail.com>
2024-03-09 13:13:16 +0100

    refactorizado de  controlador, sacar funciones para mejor visualizacion, quitar comentarios para verlos mejor

--- tweets-control.js
+++ tweets-control.js
@@ -1,52 +1,29 @@
 import { getTweets } from "./tweet-model.js";
-import { buildTweet, BuildDrawTweetsButton } from "./tweet-view.js";
+import { buildTweet} from "./tweet-view.js";
 //CONTROLADOR: es el mediador entre el modelo y la vista interactua con el dom
 
 export function tweetListController(tweetList) {
-  //1 pintar el boton
-
-  //const button = BuildDrawTweetsButton();
-  //no podemos hacer un appendChild porque es una cadena de texto
-  //pero podemos hacerlo sin funcion
+  
 
   const showButton = document.createElement('button');
   showButton.textContent = 'mostrar tweet';
   tweetList.appendChild(showButton);
-/*
-  //2 asignar evento al boton
-  showButton.addEventListener('click', () => {
-    //3 mostrar los tweets
-    getTweets()
-      .then((tweets) => {
-        tweets.forEach(tweet => {
-          const divsTweet = document.createElement('div');
-          divsTweet.innerHTML = buildTweet(tweet);
-          tweetList.appendChild(divsTweet);
-        })
-      })
-      .catch((errorMessage)=>{
-        alert(errorMessage)
-      });
 
-
-  })
-
-}
-Vamos a trabajar con async await EL ASYNC SE PONE DENTRO DE LA FUNCION DONDE SE ESTA EJECUTANDO EL AWAIT
-*/
   showButton.addEventListener('click', async ()=>{
     try {
-        const tweets = await getTweets();// espera a que la promesa cambie de estado para meterlo en la variable
-      
-      tweets.forEach(tweet => {
-        const divsTweet = document.createElement('div');
-        divsTweet.innerHTML = buildTweet(tweet);
-        tweetList.appendChild(divsTweet);
-      })
+        const tweets = await getTweets();
+        renderTweets(tweets, tweetList);
+        
     } catch (errorMessage) {
-      alert(errorMesssage)
+      alert(errorMessage);
     }
     
   })
 }
-
+function renderTweets(tweets, tweetList){
+    tweets.forEach(tweet => {
+    const divsTweet = document.createElement('div');
+    divsTweet.innerHTML = buildTweet(tweet);
+    tweetList.appendChild(divsTweet);
+})
+}
